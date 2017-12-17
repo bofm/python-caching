8d6b01d77901c5773f73c8a911012ffd07739edb
bofm <bofm@github.com>
2017-12-18 01:20:30 +0300

    bump version

--- caching/__init__.py
+++ caching/__init__.py
@@ -2,6 +2,6 @@ from .cache import Cache
 from .storage import CacheStorageBase, SQLiteStorage
 
 
-__version__ = '0.1.dev7'
+__version__ = '0.1.dev8'
 
 __all__ = (Cache, CacheStorageBase, SQLiteStorage)
